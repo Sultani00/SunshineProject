b7ea44827d017c5da176d8857f7b9c88c506bb81
Sultan Yagboyaju <sultanyagboyaju@Sultans-MacBook-Pro.local>
2015-09-06 12:32:03 -0500

    Forecast Fragment

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,17 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForeCastFragment())
                     .commit();
         }
     }
@@ -48,33 +40,11 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+}
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ArrayList<String>List=new ArrayList<>();
-            List.add("Today-Sunny-88/63");
-            List.add("Tomorrow- Foggy-70/46");
-            List.add("Weds-Cloudy-72/63");
-            List.add("thurs- Foggy-69/51");
-            List.add("Friday-Foggy-70/46");
-            List.add("Sat-Sunny-76/68");
 
-            ArrayAdapter<String>WeeklyForecast=new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_foreast_textview,List);
-
-            ListView listView=(ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(WeeklyForecast);
-
-            return rootView;
-        }
-    }
-}
