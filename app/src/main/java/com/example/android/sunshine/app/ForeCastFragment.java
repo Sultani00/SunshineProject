d1b123dcd1b58175fd3062cb33a47bcb1ca3035f
Sultan Yagboyaju <sultanyagboyaju@Sultans-MacBook-Pro.local>
2015-09-20 09:05:12 -0500

    Toast message

--- app/src/main/java/com/example/android/sunshine/app/ForeCastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForeCastFragment.java
@@ -11,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -94,9 +96,19 @@ public class ForeCastFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
         return rootView;
     }
@@ -253,7 +265,7 @@ public class ForeCastFragment extends Fragment {
                 }
                 forecastJsonStr = buffer.toString();
 
-                
+
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
